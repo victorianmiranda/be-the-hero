c63f13b699e5feed1a17be104698d196e234ae91
Victoria Miranda <vitorianevesmiranda@gmail.com>
2022-03-21 16:13:29 -0300

    login page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import react from 'react'
+
+import './global.css'
+
+import Logon from './pages/Logon'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Logon />
+  )
 }
 
 export default App;
